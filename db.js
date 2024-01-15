cefa652dfff2006afa8fa502bcba3a83841126b4
Paul Adrian <uifalean.paul96@gmail.com>
2024-01-16 00:43:28 +0200

    Test Project. TO-DO: Add Comments & Indentation.

---
+++ db.js
@@ -0,0 +1,12 @@
+// db.js
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+ host: 'localhost',
+ user: 'root',
+ password: '',
+ database: 'gestionare_produse',
+ waitForConnections: true,
+ connectionLimit: 10,
+ queueLimit: 0,
+});
+module.exports = pool;
