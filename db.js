81886323306f3253cd50877b7d8a974ce57a67fa
Paul Adrian <uifalean.paul96@gmail.com>
2024-01-16 15:58:57 +0200

    Fix Title bugs & .ejs pages. Add Indentation.

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 // db.js
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
- host: 'localhost',
- user: 'root',
- password: '',
- database: 'gestionare_produse',
- waitForConnections: true,
- connectionLimit: 10,
- queueLimit: 0,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'gestionare_produse',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
+
 module.exports = pool;
